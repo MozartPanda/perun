72f74dadb3ff5c3a0e74f1b118bb3f1acf654087
Perun CaseStudies <Perun@case_studies.com>
2024-11-11 04:26:26 +0100

    Deltadebugging fix check loop boundaries

--- tests/sources/delta_debugging_examples/dd-minimal/main.c
+++ tests/sources/delta_debugging_examples/dd-minimal/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAGIC_NUMBER 100
+#define BUFFER_SIZE 256
 
 void magicLoop() {
     for (int i = 0; i < MAGIC_NUMBER; ++i) {
@@ -28,13 +30,22 @@ int main(int argc, char* argv[]) {
     if (argc != 2) {
         return 1;
     }
+
     FILE * fp = fopen(argv[1],"r");
-    char fileContent[15];
-    int i = 0;
-    char ch;
-    while ((i < sizeof(fileContent) - 1) && (ch = fgetc(fp)) != EOF) {
-        fileContent[i++] = ch;
+
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
     }
+
+    char fileContent[BUFFER_SIZE];
+
+    while (fscanf(fp, "%255s", fileContent) == 1) {
+
+        printf("read line: %s\n", fileContent);
+    }
+
+    fclose(fp);
     checkInputString(fileContent);
     return 0;
 }
